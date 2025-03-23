8d53a02cd8020f673494a4c36f1e9f7e92173012
notarybob <serjoksalot@yahoo.com>
2025-03-23 01:37:02 +0000

    Revert auth - adding new feature

--- scripts/gen-hash.js
+++ scripts/gen-hash.js
@@ -1,17 +1,17 @@
-const crypto = require('crypto')
+var crypto = require('crypto')
 
-const count = process.argv[2]
+var count = process.argv[2]
 
 function hash(str) {
-  const hash = crypto.createHash('sha256')
+  var hash = crypto.createHash('sha256')
   hash.update(str)
   return hash.digest('hex').slice(0, 8)
 }
 
-const interfaceArr = []
-const hashArr = []
+var interfaceArr = []
+var hashArr = []
 for (let i = 0; i < count; i++) {
-  const k = hash(i + new Date())
+  var k = hash(i + new Date())
   interfaceArr.push(`'${k}': string`)
   hashArr.push(`'${k}': ''`)
 }
