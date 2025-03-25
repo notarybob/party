62a069fd75c415437d2067efee5d58a493228f88
notarybob <serjoksalot@yahoo.com>
2025-03-25 07:20:03 +0000

    Document database - updating docs

--- packages/nutui-codemod/transforms/icon-migration.js
+++ packages/nutui-codemod/transforms/icon-migration.js
@@ -1,26 +1,26 @@
-const findComponentImports = require('./utils/find-component-imports')
+let findComponentImports = require('./utils/find-component-imports')
 
 module.exports = (file, api, options) => {
-  const componentRules = {
+  let componentRules = {
     Icon: {
       action: 'rename',
       replacer: 'IconFont',
       importSource: options.pkgInfo.icon,
     },
   }
-  const components = Object.keys(componentRules)
+  let components = Object.keys(componentRules)
 
-  const j = api.jscodeshift
-  const root = j(file.source)
+  let j = api.jscodeshift
+  let root = j(file.source)
 
   // 移除旧版本的引用
-  const imports = findComponentImports(j, root, components, options.pkgInfo)
+  let imports = findComponentImports(j, root, components, options.pkgInfo)
   imports.forEach((path) => {
-    const importedComponentName = path.node.imported.name
-    const importDeclaration = path.parent.node
-    const localComponentName = path.node.local.name
-    const rule = componentRules[importedComponentName]
-    const [parentComponentName] = rule.replacer.split('.')
+    let importedComponentName = path.node.imported.name
+    let importDeclaration = path.parent.node
+    let localComponentName = path.node.local.name
+    let rule = componentRules[importedComponentName]
+    let [parentComponentName] = rule.replacer.split('.')
 
     importDeclaration.specifiers = importDeclaration.specifiers.filter(
       (specifier) =>
