b05b4612818847cf77533f3d10bedd97452a8066
notarybob <serjoksalot@yahoo.com>
2025-03-25 06:26:07 +0000

    Remove README - adding new feature

--- scripts/generate-changelog.js
+++ scripts/generate-changelog.js
@@ -1,17 +1,17 @@
-const fs = require('fs-extra');
-const path = require('path');
-const changelog = fs.readFileSync(path.join(__dirname, '../CHANGELOG.md'), 'utf8');
+var fs = require('fs-extra');
+var path = require('path');
+var changelog = fs.readFileSync(path.join(__dirname, '../CHANGELOG.md'), 'utf8');
 
-const a = changelog.split('# v')
+var a = changelog.split('# v')
 console.log(a[1])
-const getLatestRelease = (cl) => {
-  const a = changelog.split('# v')
+var getLatestRelease = (cl) => {
+  var a = changelog.split('# v')
   if(a.length === 0) return ''
   return `# v${a[1]}`
 
-  // const tag1 = cl.indexOf('#')
+  // var tag1 = cl.indexOf('#')
   // if (tag1 > -1) {
-  //   const tag2 = cl.split('').slice(tag1 + 1, cl.length - 1).join('').indexOf('#')
+  //   var tag2 = cl.split('').slice(tag1 + 1, cl.length - 1).join('').indexOf('#')
   //   if (tag2 > -1) {
   //     return cl.substring(tag1, tag2)
   //   }
