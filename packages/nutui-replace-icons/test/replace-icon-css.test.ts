2b827586bb2c92074da00e53c3e95c18d383aedc
notarybob <serjoksalot@yahoo.com>
2025-03-25 10:03:49 +0000

    Update build - improving performance

--- packages/nutui-replace-icons/test/replace-icon-css.test.ts
+++ packages/nutui-replace-icons/test/replace-icon-css.test.ts
@@ -2,23 +2,23 @@ import babel from '@babel/core'
 import { describe, expect, it } from 'vitest'
 import { replaceIcons } from '../src/replace-icons'
 
-let plugin = replaceIcons({
+const plugin = replaceIcons({
   sourceLibrary: ['@nutui/icons-react', '@nutui/icons-react-taro'],
   targetLibrary: '@test/aa',
 })
 
-let babelOptions = {
+const babelOptions = {
   presets: ['@babel/preset-react'],
   plugins: [plugin],
 }
-let caseIns = `
+const caseIns = `
 import '@nutui/icons-react/dist/test.css'
 import '@nutui/icons-react-taro/dist/test.css'
 import '@nutui/taro/dist/test.css'
 `
 describe('', () => {
   it('replace Loading icons with Star', () => {
-    let ast = babel.transformSync(caseIns, babelOptions)
+    const ast = babel.transformSync(caseIns, babelOptions)
     // @ts-ignore
     expect(ast.code).toMatchSnapshot()
   })
